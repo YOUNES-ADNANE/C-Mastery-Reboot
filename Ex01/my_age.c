097737c40bfeaff875ef1e584aee95e2790afbe8
YOUNES ADNANE <younessadnane1m@gmail.com>
2026-04-16 19:36:32 +0100

    ex01: migrated to Vim & Terminal with pure write logic

---
+++ Ex01/my_age.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   my_age.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: younesadnane <younesadnane1337@gmail.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/16 19:18:36 by younesadnane      #+#    #+#             */
+/*   Updated: 2026/04/16 19:21:13 by younesadnane     ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(void)
+{
+	int age = 17;
+
+	write(1, "My age is: ",11);
+
+
+	ft_putchar((age / 10) + '0');
+
+	ft_putchar((age % 10) + '0');
+
+	ft_putchar('\n');
+
+	return (0);
+}
